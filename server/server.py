ad7347c11f2dca9e91a94cd0d59a1ff80b4264b0
darrenlimkh <darrenkhlim@live.com.sg>
2023-03-12 13:49:54 +0800

    added API fetch from frontend to backend

--- server/server.py
+++ server/server.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/data")
-def data():
-    return {"data": ["data1", "data2", "data3"]}
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.get_json()["userid"]
+    password = request.get_json()["pass"]
+    print(username, password)
+    try:
+        return jsonify(
+            {
+            "message": "Login Successful.",
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Login Failed."
+        }), 500
+    
+@app.route("/register", methods=['POST'])
+def register():
+    username = request.get_json()["userid"]
+    password = request.get_json()["pass"]
+    print(username, password)
+    try:
+        return jsonify(
+            {
+            "message": "Registeration Successful.",
+            }
+        ), 200
+    except Exception as e:
+        return jsonify({
+            "message": "Create user failed."
+        }), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
