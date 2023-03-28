f12c3371afa2f23188ab66477b6ef841316ef97f
Ng Zhi Qing <winnie2code@gmail.com>
2023-03-28 23:55:59 +0800

    update server.py

--- server/server.py
+++ server/server.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from database.RDSdatabase import RDSdatabase
 
 app = Flask(__name__)
 cors = CORS(app)
+
+#not sure where to put this. Initial database configuration
+masterUserName = "masteruser"
+masterPassword = "hishmaster123"
+host = "hish-db-01.cfwyts8tlkjs.us-east-1.rds.amazonaws.com"
+dbName = "postgres"
+postgresPort = 5432
+rdsDB = RDSdatabase(masterUserName,masterPassword,host,dbName,postgresPort)
+#initial config
+rdsDB.initialConfig()
 
 @app.route("/login", methods=['POST'])
 def login():
@@ -19,6 +29,11 @@ def login():
     # } else {
     #     # TODO: handle register
     # }
+    if not isLogin: 
+        #register user
+        rdsDB.createNewUser(username, password, organisation)
+    #user sign in 
+    userEngine, userCursor = rdsDB.userSignIn(username, password)
     
     try:
         return jsonify(
